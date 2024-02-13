adcc399ce3657cd46507c97ab7a194abc8ce85d2
Nurlan <iamsamurai1991@gmail.com>
2024-02-13 21:18:53 +0100

    modified:   .env 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/schema.prisma 	modified:   src/app/api/auth/[...nextauth]/route.ts 	modified:   src/utils/auth.tsx 	new file:   src/utils/connect.tsx

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import { authOptions } from "@/utils/auth";
 import NextAuth from "next-auth";
-
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
