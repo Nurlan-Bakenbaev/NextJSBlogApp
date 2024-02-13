0dc257ed2839234529909d4aba1c74a9b754d3be
Nurlan <iamsamurai1991@gmail.com>
2024-02-13 22:34:40 +0100

    modified:   .env 	modified:   prisma/schema.prisma 	modified:   src/app/api/auth/[...nextauth]/route.ts 	deleted:    src/utils/auth.tsx 	deleted:    src/utils/connect.tsx

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,22 @@
-import { authOptions } from "@/utils/auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-const handler = NextAuth(authOptions);
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+
+const prisma = new PrismaClient();
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+});
 export { handler as GET, handler as POST };
