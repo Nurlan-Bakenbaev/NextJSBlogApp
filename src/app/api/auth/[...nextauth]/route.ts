df1b977dbb59e52281dce4025ef6d7983d5f38df
Nurlan <iamsamurai1991@gmail.com>
2024-02-14 23:38:03 +0100

    new file:   prisma/schema.prisma 	modified:   src/app/api/auth/[...nextauth]/route.ts 	new file:   src/utils/prisma.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,19 @@
 import { checkDatabaseConnection } from "@/utils/db";
-
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 checkDatabaseConnection();
 
 const handler = NextAuth({
+ 
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
-  ],
+  ], adapter: PrismaAdapter(prisma),
 });
 export { handler as GET, handler as POST };
